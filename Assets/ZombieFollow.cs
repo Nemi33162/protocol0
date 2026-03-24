71224e04f4722ca0e41ee08abe620479130f69a6
Nehemiah <nemi33162@gmail.com>
2026-03-24 01:05:12 -0400

    progress update

---
+++ Assets/ZombieFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public class ZombieFollow : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 2f;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
